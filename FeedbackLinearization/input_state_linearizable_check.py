ce26130c9f91b210f05e679a59b54f130442dd75
Mahika Goel <mahikagoel7@gmail.com>
2024-04-03 20:26:07 -0400

    feedback linearizable check + test case

---
+++ FeedbackLinearization/input_state_linearizable_check.py
@@ -0,0 +1,44 @@
+# CLASS TO DETERMINE IF SYSTEM IS INPUT STATE LINEARIZABLE
+import sympy as sym
+
+class InputStateLinearizableCheck:
+
+    def __init__(self, f, g, n, state):
+        self.f = f # f dynamics matrix
+        self.g = g # g input matrix
+        self.n = n # number of states
+        self.states = state
+        self.vfield = sym.Matrix.zeros(n, n)
+        pass
+
+    def liebracket(self, f_v, g_v, i):
+
+        if i == 0:
+            return g_v
+        elif i == 1:
+            return g_v.jacobian(self.states) @ f_v - f_v.jacobian(self.states) @ g_v
+        else:
+            bracket = self.liebracket(f_v, g_v, i-1)
+            return self.liebracket(f_v, bracket, 1)
+
+    def vector_field(self):
+        for i in range(self.n):
+            self.vfield[:, i] = self.liebracket(self.f, self.g, i)
+        return self.vfield
+
+    def input_state_linearizable_check(self):
+        controllability_bool = False
+        involutive_bool = False
+
+        print(self.vector_field())
+
+        vfield = sym.Matrix(self.vfield)
+        rank = vfield.rank()
+
+        if (rank == self.n):
+            controllability_bool = True
+
+        # return controllability_bool*involutive_bool
+        return controllability_bool
+
+
---
+++ FeedbackLinearization/test_case_feedback_lin.py
@@ -0,0 +1,24 @@
+import sympy as sym
+from input_state_linearizable_check import InputStateLinearizableCheck
+
+x1, x2, x3, x4 = sym.symbols('x1, x2, x3, x4')
+k, I, J, M, g, L = sym.symbols('k, I, J, M, g, L')
+
+f = sym.Matrix([x2, -(M*g*L/I)*sym.sin(x1) - (k/I)*(x1-x3), x4, (k/J)*(x1-x3)])
+
+g = sym.Matrix([0, 0, 0, 1/J])
+
+states = sym.Matrix([x1, x2, x3, x4])
+
+n = 4
+
+input_linearizer = InputStateLinearizableCheck(f, g, n, states)
+# lie_bracket = input_linearizer.liebracket(1)
+# print(lie_bracket)
+
+linearizable_bool = input_linearizer.input_state_linearizable_check()
+print(linearizable_bool)
+
+# see Slotine Pg. 239 for double checking of vector fields & controllability result
+
+
